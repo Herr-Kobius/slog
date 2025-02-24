010088cd4b3ea1a7b0bf5ba592c3aed4c4c9b7a3
Jacob Vogler <119408574+Herr-Kobius@users.noreply.github.com>
2025-02-24 11:58:05 +0100

    Add log_function_call

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ import os
 import logging
 logs_folder = './logs'
 
+global_logger = Logger('All', logs_folder, console_level=logging.DEBUG)
+print(f'Log File Time Stemp:{global_logger.file_name}')
 
 def remove_logs(path:str) -> None:
     for file_name in sorted(os.listdir(path)):
@@ -10,7 +12,9 @@ def remove_logs(path:str) -> None:
             os.remove(os.path.join(path, file_name))
 
 
-if __name__ != "__main__":
-    global_logger = Logger('All', logs_folder, console_level=logging.DEBUG)
-    print(f'Log File Time Stemp:{global_logger.file_name}')
-
+def log_function_call(func):
+    def wrap(*args, **kwargs):
+        global_logger.info(f"Called {func.__name__} with {args} {kwargs}")
+        result = func(*args, **kwargs)
+        return result
+    return wrap
--- logger.py
+++ logger.py
@@ -7,8 +7,8 @@ class Logger(logging.Logger):
     def __init__(self, name: str, path:str, console_level:int=logging.WARNING, file_level:int=logging.DEBUG, file_suffix:str='') -> None:
         super().__init__(name, logging.DEBUG)
 
-        sh_formatter = ColorFormatter('%(asctime)s.%(msecs)02d [%(name)s] %(levelname)s %(message)s', '%H:%M:%S')
-        fh_formatter = logging.Formatter('%(asctime)s.%(msecs)02d [%(name)s] %(levelname)s %(message)s', '%m-%d %H:%M:%S')
+        sh_formatter = ColorFormatter('%(asctime)s.%(msecs)03d [%(name)s] %(levelname)s %(message)s', '%H:%M:%S')
+        fh_formatter = logging.Formatter('%(asctime)s.%(msecs)03d [%(name)s] %(levelname)s %(message)s', '%m-%d %H:%M:%S')
 
         sh = logging.StreamHandler()
         sh.setLevel(console_level)
